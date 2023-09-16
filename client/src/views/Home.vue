c0233f94ccb068fe87b46ce0515ff0207d1359df
mrjex <joel.mattsson@hotmail.se>
2023-09-16 17:28:56 +0200

    Fix username-display bug

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,33 +1,38 @@
 <template>
-  <div>
-    <h3 v-if="user">Hi, {{user.username}}</h3>
-    <h3 v-if="!user">You are not logged in</h3>
-  </div>
+    <form @submit.prevent="handleSubmit">
+        <h3>Profile!</h3>
+        <button class="btn btn-primary btn-block">Profile</button>
+
+         <div>
+           <h3 v-if="user">Hi, {{user}}</h3>
+           <h3 v-if="!user">You are not logged in</h3>
+        </div>
+    </form>
+
 </template>
 
 <script>
 // @ is an alias to /src
 import { Api } from '@/Api'
-// import axios from 'axios'
 
 export default {
   name: 'home',
   data() {
     return {
-      user: null
+      user: null //
     }
   },
-  /*
   async created() {
-    const response = await axios.get('user', {
-      headers: {
-        Authorization: 'Bearer' + localStorage.getItem('password')
-      }
-    })
+    const usernameLength = localStorage.getItem('logged-in-username').length
 
-    this.user = response.data
+    if (usernameLength > 0) {
+      this.user = localStorage.getItem('logged-in-username')
+    } else {
+      this.user = null
+    }
+
+    console.warn(this.user)
   },
-  */
   methods: {
     getMessage() {
       Api.get('/')
