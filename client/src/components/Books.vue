7c938685840f731bb7f800288bb64820fc1f029d
mohamadk <johnmurphyz166@gmail.com>
2023-09-20 12:56:41 +0200

    bug fixes to books page

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -1,18 +1,53 @@
 <template>
-        <form @submit.prevent="handleBookSubmit">
-        <h3>{{user}}'s books:</h3>
-        <h3>{{books}}</h3>
-    </form>
+  <form @submit.prevent="handleBookSubmit">
+    <b-container>
+      <h3>{{ user }}'s books:</h3>
+      <b-row v-for="result in books" :key=result.id class="my-4">
+        <Book :book=result>
+          <b-button v-b-modal.modal-1 variant="danger">Remove from
+            library</b-button>
+          <b-modal id="modal-1" title="Are you sure?" ok-variant="danger" @ok="() => removeBook(result)">
+            <p class="my-4">This will remove the book from your library</p>
+          </b-modal>
+        </Book>
+      </b-row>
+    </b-container>
+  </form>
 </template>
 
 <script>
 import axios from 'axios'
+import Book from './Book'
+
 export default {
   name: 'Books',
+  components: { Book },
   data() {
     return {
       user: null,
-      books: null
+      books: []
+    }
+  },
+  methods: {
+    async removeBook(book) {
+      try {
+        await axios.delete(`http://localhost:3000/api/users/mrGit/books/${book.isbn}`)
+        this.$bvToast.toast('Success', {
+          title: 'Removed book from library',
+          autoHideDelay: 5000,
+          appendToast: false
+        })
+        this.books = this.books.filter(entry => entry.isbn !== book.isbn)
+      } catch {
+        this.$bvToast.toast('Error', {
+          title: 'Something went wrong',
+          autoHideDelay: 5000,
+          appendToast: false
+        })
+      }
+    },
+    logger() {
+      console.log('I was clicked')
     }
   },
   async created() {
@@ -29,5 +64,6 @@ export default {
 
     localStorage.setItem('current-page', 'Books')
   }
+
 }
 </script>
