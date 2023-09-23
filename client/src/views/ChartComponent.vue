c3c8f97f133bb9ffa47684cf2868a088fa04e889
mrjex <joel.mattsson@hotmail.se>
2023-09-23 23:22:55 +0200

    Add functionality such that charts work simultaneously on seperate port

---
+++ client/src/views/ChartComponent.vue
@@ -0,0 +1,7 @@
+<template>
+    <body>
+        <div style="width: 500px;"><canvas id=radarchart></canvas></div>
+        <script type="javascript" src="./radarchart.js"></script>
+        <!-- <script type="javascript" src="radarchart.js"></script> THIS IS THE ERROR -->
+    </body>
+</template>
