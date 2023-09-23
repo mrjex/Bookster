64a4825238b25bb9b5a9a3cf0433fbccae980ed4
joelmat <joelmat@student.chalmers.se>
2023-09-23 23:22:55 +0200

    Add functionality such that charts work simultaneously on seperate port

--- client/src/views/Progress.vue
+++ client/src/views/Progress.vue
@@ -1,12 +1,20 @@
 <template>
+<div>
+  <!--
+  <div style="width: 500px;"><canvas id=radarchart></canvas></div>
+  <script type="module" src="../radarchart.js"></script>
+  -->
+  <!-- <ChartComponent /> -->
   <form @submit.prevent="handleBookSubmit">
     <h3>{{ user }}'s progress:</h3>
     <h3>{{ progress }}</h3>
   </form>
+</div>
 </template>
 
 <script>
 import { Api } from '../Api'
+// import ChartComponent from './ChartComponent.vue'
 export default {
   name: 'ProgressPage',
   inject: ['user'],
@@ -18,6 +26,10 @@ export default {
   async created() {
     const result = await Api.get(`/users/${this.user}/progress`)
     this.progress = result.data
+    console.warn('PROGRESS PAGE!')
+  },
+  components: {
+    // ChartComponent,
   }
 }
 </script>
