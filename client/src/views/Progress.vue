6eff22bd4a1fbfad0c45c58054c97cfe7d3dd398
mrjex <joel.mattsson@hotmail.se>
2023-09-24 21:28:11 +0200

    Add charts on 'progress' page with arbitrary data

--- client/src/views/Progress.vue
+++ client/src/views/Progress.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
-    <!--
-    <div style="width: 500px;"><canvas id=radarchart></canvas></div>
-    <script type="module" src="../charts/radarchart.js"></script>
-    -->
-    <ChartComponent />
-    <!--
-    <form @submit.prevent="handleBookSubmit">
-      <h3>{{ user }}'s progress:</h3>
-      <h3>{{ progress }}</h3>
-    </form>
-    -->
+    <h3>{{ user }}'s progress:</h3>
+    <h3>{{ progress }}</h3>
+    <PieChart />
+    <BarChart />
+    <LineChart />
+    <PolarChart />
+    <RadarChart />
   </div>
 </template>
 
 <script>
-// import { Api } from '../Api'
-import ChartComponent from './ChartComponent.vue'
+import { Api } from '../Api'
+import RadarChart from './charts/RadarChart.vue'
+import PolarChart from './charts/PolarChart.vue'
+import LineChart from './charts/LineChart.vue'
+import BarChart from './charts/BarChart.vue'
+import PieChart from './charts/PieChart.vue'
+
 export default {
   name: 'ProgressPage',
   inject: ['user'],
@@ -25,15 +26,17 @@ export default {
       progress: null
     }
   },
-  /*
   async created() {
     const result = await Api.get(`/users/${this.user}/progress`)
     this.progress = result.data
     console.warn(this.progress)
   },
-  */
   components: {
-    ChartComponent
+    RadarChart,
+    PolarChart,
+    LineChart,
+    BarChart,
+    PieChart
   }
 }
 </script>
