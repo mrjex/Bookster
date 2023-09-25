31ab1e8dfce2602c6134f0d47d17bdf6ebbc17f7
mrjex <joel.mattsson@hotmail.se>
2023-09-25 10:31:06 +0200

    Attempt to integrate synchronized addition of new data points in multiple charts

---
+++ client/src/views/charts/performance/PerformanceChartsComponent.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <LineChart />
+    <BarChart />
+
+    <h2>Performance Charts Component</h2>
+
+        <form @submit.prevent="handleSubmit">
+
+        <h3>Add new data:</h3>
+        <div class="form-group">
+            <label>Day label</label>
+            <input type="dayLabel" class="form-control" v-model="dayLabel" placeholder="Day label"/>
+        </div>
+
+        <div class="form-group">
+            <label>Data</label>
+            <input type="dataValue" class="form-control" v-model="dataValue" placeholder="Data"/>
+        </div>
+
+        <button class="btn btn-primary btn-block">Submit Data </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import LineChart from './LineChart.vue'
+import BarChart from './BarChart.vue'
+
+export default {
+  name: 'PerformanceChartsComponent',
+  provide() {
+    return { chartDataTest: this.chartData, chartOptionsTest: this.chartOptions }
+  },
+  data() {
+    return {
+      chartData: {
+        labels: [
+          'Day 1',
+          'Day 2',
+          'Day 3',
+          'Day 4',
+          'Day 5',
+          'Day 6',
+          'Day 7',
+          'Day 8',
+          'Day 9',
+          'Day 10',
+          'Day 11',
+          'Day 12'
+        ],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 39, 10, 40, 39, 73, 40, 20, 7, 3, 24, 37]
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  },
+  components: {
+    LineChart,
+    BarChart
+  }
+}
+</script>
