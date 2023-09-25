732353d2c41c32a83c1b386446de388f0c26e10a
mohamadk <johnmurphyz166@gmail.com>
2023-09-25 10:48:57 +0200

    pass data from progress page

--- client/src/views/Progress.vue
+++ client/src/views/Progress.vue
@@ -2,8 +2,9 @@
   <div>
     <h3>{{ user }}'s progress:</h3>
     <h3>{{ progress }}</h3>
-    <BarChart />
-    <LineChart />
+    <BarChart :chartData="chartData" />
+    <LineChart :chartData="chartData" />
+    <b-button @click.prevent="addStuff">Add Lol</b-button>
     <PolarChart />
     <RadarChart />
     <PieChart />
@@ -23,7 +24,30 @@ export default {
   inject: ['user'],
   data() {
     return {
-      progress: null
+      progress: null,
+      chartData: {
+        labels: [
+          'Day 1',
+          'Day 2',
+          'Day 3',
+          'Day 4',
+          'Day 5',
+          'Day 6',
+          'Day 7',
+          'Day 8',
+          'Day 9',
+          'Day 10',
+          'Day 11',
+          'Day 12'
+        ],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+          }
+        ]
+      }
     }
   },
   async created() {
@@ -37,6 +61,12 @@ export default {
     LineChart,
     BarChart,
     PieChart
+  },
+  methods: {
+    addStuff() {
+      this.chartData.labels.push('Day 13')
+      this.chartData.datasets[0].data.push(60)
+    }
   }
 }
 </script>
