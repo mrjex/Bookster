64c4a99e9a3544616a36619ce66e31d31468d162
mrjex <joel.mattsson@hotmail.se>
2023-09-27 09:08:09 +0200

    Remove redundant schema attributes

--- server/models/progress.js
+++ server/models/progress.js
@@ -16,9 +16,6 @@ const progressSchema = mongoose.Schema(
         allocationChartsLastMonth: {
             type: [Number]
         },
-        allocationChartPie: {
-            type: [Number]
-        },
         allocationCategories: {
             type: [String]
         },
