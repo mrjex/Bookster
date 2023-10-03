e4ddc4249f36b09608a17b601518ea189db1ead7
mohamadk <mohamadk@student.chalmers.se>
2023-10-03 09:17:22 +0200

    add delete account functionality and some styling fixes

--- client/src/views/User.vue
+++ client/src/views/User.vue
@@ -1,25 +1,24 @@
 <template>
-    <div>
-        <h3>This is your profile, {{user}}</h3>
-        <div>
-            <button class="btn btn-primary btn-block"
-            @click.prevent="booksButton">Your Books</button>
-        </div>
+  <div>
+    <b-container>
 
-        <div>
-            <button class="btn btn-primary btn-block"
-            @click.prevent="reviewsButton">Your Reviews</button>
-        </div>
+      <h3>This is your profile, {{ user }}</h3>
+      <button class="btn btn-primary btn-block" @click.prevent="booksButton">Your Books</button>
+      <button class="btn btn-primary btn-block" @click.prevent="reviewsButton">Your Reviews</button>
+      <button class="btn btn-primary btn-block" @click.prevent="progressButton">Your Progress</button>
+      <button class="btn btn-danger btn-block" v-b-modal.modal-1>Delete Account</button>
 
-        <div>
-            <button class="btn btn-primary btn-block"
-            @click.prevent="progressButton">Your Progress</button>
-        </div>
-    </div>
+      <b-modal id="modal-1" title="Delete Account" ok-variant="danger" @ok="deleteAccount">
+        <p class="my-4">This will delete your account permanently</p>
+      </b-modal>
+
+    </b-container>
+  </div>
 </template>
 
 <script>
 import UtilsComponent from '../components/UtilsComponent.vue'
+import { Api } from '../Api'
 export default {
   name: 'User',
   inject: ['user'],
@@ -36,6 +35,11 @@ export default {
     progressButton() {
       UtilsComponent.methods.setRefreshablePageState()
       this.$router.push(`${this.$route.path}/progress`)
+    },
+    async deleteAccount() {
+      localStorage.removeItem('logged-in-username')
+      await Api.delete(`/users/${this.user}`)
+      this.$router.push('/login')
     }
   }
 }
