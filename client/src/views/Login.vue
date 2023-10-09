a1912657584a21282e8e89f18d36f63a65acb317
mrjex <joel.mattsson@hotmail.se>
2023-10-09 23:22:53 +0200

    Fix styling for sign-up page

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -8,21 +8,25 @@
           </div>
 
           <h4 class="no-account">Don't have an account?</h4>
-          <div class="form-group">
-            <svg xmlns="http://www.w3.org/2000/svg" class="user-icon" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
-              <input type="username" class="input-btn" v-model="username" placeholder="Username"/>
-          </div>
+          <h4 class="welcome-text">WELCOME</h4>
 
-          <div class="form-group">
-            <svg xmlns="http://www.w3.org/2000/svg" class="password-icon" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/></svg>
-              <input type="password" class="input-btn" v-model="password" placeholder="Password"/>
-          </div>
+          <div class="form-frontend">
+            <div class="form-group">
+              <svg xmlns="http://www.w3.org/2000/svg" class="user-icon" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+                <input type="username" class="input-btn" v-model="username" placeholder="Username"/>
+            </div>
 
-          <button class="input-btn login-btn">LOGIN</button>
+            <div class="form-group">
+              <svg xmlns="http://www.w3.org/2000/svg" class="password-icon" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/></svg>
+                <input type="password" class="input-btn" v-model="password" placeholder="Password"/>
+            </div>
+
+            <button class="input-btn login-btn">LOGIN</button>
+          </div>
       </form>
 
       <a href="/register">
-        <button class="input-btn signup-btn">Sign up</button>
+        <button class="input-btn signup2-btn">Sign up</button>
       </a>
       <img src="../resources/LoginCardPicture1.png" class="card0-background">
     </div>
@@ -129,7 +133,7 @@ export default {
   left: 3.7vw;
 }
 
-.signup-btn {
+.signup2-btn {
   position: relative;
   left: 45vw;
   top: 30vh;
@@ -145,6 +149,15 @@ export default {
   left: 1.4vw;
 }
 
+.welcome-text {
+  color: #ffffff;
+  font-weight: 500;
+  font-size: 20px;
+  position: relative;
+  top: -12.5vh;
+  left: 3.36vw;
+}
+
 .user-icon {
   position: relative;
   top: 3.2vh;
@@ -155,5 +168,11 @@ export default {
   position: relative;
   top: 3.2vh;
   left: -1.35vw;
+}
+
+.form-frontend {
+  position: relative;
+  top: -6vh;
+  left: 0.4vw;
 }
 </style>
