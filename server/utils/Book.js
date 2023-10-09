2a3326a39ca9b75375b20820e00193cd84c34fb4
Mohamad Khalil <johnmurphyz166@gmail.com>
2023-10-09 22:25:33 +0200

    fix bugs with books page

--- server/utils/Book.js
+++ server/utils/Book.js
@@ -10,16 +10,18 @@ module.exports = class Book {
     ratingsCount = 0;
 
     constructor(data) {
+        
         this.id = data.id;
         this.title = data.volumeInfo.title;
         this.description = data.volumeInfo.description?.replace(/<\/?[^>]+(>|$)/g, "");
         this.pages = data.volumeInfo.pageCount;
         this.publishYear = data.volumeInfo.publishedDate;
         this.ratingsCount = data.volumeInfo.ratingsCount || 0;
-        if (data.volumeInfo.imageLinks) {
-            this.coverURL = data.volumeInfo.imageLinks.thumbnail;
-        }
-        
+        this.coverURL = data.volumeInfo.imageLinks?.thumbnail || 'https://i.pinimg.com/1200x/f4/fa/8a/f4fa8a9e155d94cce553cffdc247b493.jpg';
+        // if(data.volumeInfo.imageLinks?.extraLarge) {
+        //     this.coverURL = data.volumeInfo.imageLinks?.extraLarge
+        // }
+
     }
 
 }
