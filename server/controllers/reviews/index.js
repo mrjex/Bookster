0e29f1c8ee56a29ad11cb252df410180ab50429d
Mohamad Khalil <johnmurphyz166@gmail.com>
2023-10-14 11:01:57 +0200

    add get request for all reviews

--- server/controllers/reviews/index.js
+++ server/controllers/reviews/index.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const Review = require('../../models/review');
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+
+    const reviews = await Review.find({});
+    res.json(reviews)
+
+})
 
 module.exports = router;
