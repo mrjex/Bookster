615393123871c4a3b4c53e564f44e29ede5614b2
Mohamad Khalil <johnmurphyz166@gmail.com>
2023-10-15 12:13:56 +0200

    improvement to book component responsiveness

--- client/src/components/Book.vue
+++ client/src/components/Book.vue
@@ -1,21 +1,21 @@
 <template>
-    <b-card no-body class="overflow-hidden text-left" role="button" @click="this.viewBook">
-      <b-row no-gutters>
-        <b-col md="2">
-          <b-card-img :src="book.coverURL" class="book-cover rounded-0"></b-card-img>
-        </b-col>
-        <b-col md="10">
-          <b-card-body :title="book.title">
-            <b-card-text>
-              <p>
-                {{ book.description || 'No description available' }}
-              </p>
-            </b-card-text>
-            <slot></slot>
-          </b-card-body>
-        </b-col>
-      </b-row>
-    </b-card>
+  <b-card no-body class="overflow-hidden text-left" role="button" @click="this.viewBook">
+    <b-row no-gutters>
+      <b-col class="cover-container" md="2">
+        <b-card-img :src="book.coverURL" class="book-cover rounded-0"></b-card-img>
+      </b-col>
+      <b-col md="10">
+        <b-card-body :title="book.title">
+          <b-card-text>
+            <p>
+              {{ book.description || 'No description available' }}
+            </p>
+          </b-card-text>
+          <slot></slot>
+        </b-card-body>
+      </b-col>
+    </b-row>
+  </b-card>
 </template>
 <script>
 export default {
@@ -30,7 +30,13 @@ export default {
 </script>
 
 <style>
-.book-cover {
-  width:200px
+
+@media only screen and (max-width: 768px) {
+  .book-cover {
+    width: 150px !important;
+  }
+  .cover-container {
+    text-align: center;
+  }
 }
 </style>
