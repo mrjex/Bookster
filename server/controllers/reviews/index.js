5f613c1264d519036fe2e6e32e6e1a49c22c5e1c
Mohamad Khalil <johnmurphyz166@gmail.com>
2023-10-15 11:48:48 +0200

    add put method for reviews

--- server/controllers/reviews/index.js
+++ server/controllers/reviews/index.js
@@ -2,41 +2,55 @@ const express = require('express');
 const Review = require('../../models/review');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
 
     try {
         const reviews = await Review.find({});
         res.json(reviews)
     }
-    catch(error) {
+    catch (error) {
         next(error)
     }
 
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
 
     try {
         const id = req.params.id;
         await Review.deleteOne({ _id: id });
         res.sendStatus(200)
     }
-    catch(error) {
+    catch (error) {
         next(error)
     }
-    
+
 })
 
-router.delete('/', async (req, res) => {
+router.delete('/', async (req, res, next) => {
 
     try {
         await Review.deleteMany({});
         res.sendStatus(200)
     }
-    catch(error) {
+    catch (error) {
         next(error)
     }
-    
+
+})
+
+router.put('/:id', async (req, res, next) => {
+
+    try {
+        const { id } = req.params;
+        const payload = req.body;
+        const result = await Review.findOneAndUpdate({ _id: id }, payload);
+        res.status(201).json(result)
+    }
+    catch (error) {
+        next(error)
+    }
+
 })
 
 module.exports = router;
